692b4785094603609ac5b4487bf95efc07ac9b53
Chris Evans <chrisevans9629@gmail.com>
2020-03-16 18:47:03 -0500

    added unity

---
+++ DotDodge/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float Score;
+    public Text text;
+    public float Jump;
+    //public float Speed;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+         {
+            rb.AddForce(0, Jump * Time.deltaTime,0, ForceMode.Impulse);
+        }
+
+        Score++;
+        text.text = "Score: " + Score;
+    }
+}
---
+++ DotDodge/Assets/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float MaxY;
+    public float MinY;
+    public float MaxSeconds;
+    public float MinSeconds;
+    public float MinSpeed;
+    public float MaxSpeed;
+    public GameObject EnemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(MinSeconds, MaxSeconds));
+
+            var position = transform.position;
+            position.y = Random.Range(MinY, MaxY);
+
+            var result = Instantiate(EnemyPrefab, position, Quaternion.identity, transform);
+            result.GetComponent<Enemy>().Speed = Random.Range(MinSpeed, MaxSpeed);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
