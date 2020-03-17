7bd6199081a6b213375b4864434eddc5e3f56114
Chris Evans <chrisevans9629@gmail.com>
2020-03-17 17:36:52 -0500

    added new input system

--- DotDodge/Assets/Player.cs
+++ DotDodge/Assets/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -16,13 +17,44 @@ public class Player : MonoBehaviour
     public float FireRateSeconds;
     public UnityEvent PlayerDied;
     public GameObject Bullet;
+
+    private PlayerInputActions input;
     // Start is called before the first frame update
+    void Awake()
+    {
+        input = new PlayerInputActions();
+        input.Player.Jump.performed += JumpOnPerformed;
+    }
+
+    private void JumpOnPerformed(InputAction.CallbackContext obj)
+    {
+        rb.AddForce(0, Jump * Time.deltaTime, 0, ForceMode.Impulse);
+    }
+
     void Start()
     {
         StartCoroutine(Fire());
         Time.timeScale = 1;
+
     }
 
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+
+    void OnDestroy()
+    {
+        input.Player.Jump.performed -= JumpOnPerformed;
+
+        input.Dispose();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Bullet"))
@@ -43,10 +75,9 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(0, Jump * Time.deltaTime, 0, ForceMode.Impulse);
-        }
+        //if (input.Player.Jump.ReadValue<float>() > 0)
+        //{
+        //}
 
         if (Time.timeScale > 0)
         {
