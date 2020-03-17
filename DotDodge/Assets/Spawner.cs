0f69e6d1f7f1cae166c9e242cd96199997d6b746
Chris Evans <chrisevans9629@gmail.com>
2020-03-16 19:31:09 -0500

    added increasing difficulty

--- DotDodge/Assets/Spawner.cs
+++ DotDodge/Assets/Spawner.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
@@ -11,9 +13,13 @@ public class Spawner : MonoBehaviour
     public float MinSpeed;
     public float MaxSpeed;
     public GameObject EnemyPrefab;
+    public float DifficultyEveryScore = 5000;
+    public float DifficultyIncrement = 1;
+    public Player Player;
     // Start is called before the first frame update
     void Start()
     {
+        nextDifficulty = DifficultyEveryScore;
         StartCoroutine(Spawn());
     }
 
@@ -28,9 +34,23 @@ public class Spawner : MonoBehaviour
 
             var result = Instantiate(EnemyPrefab, position, Quaternion.identity, transform);
             result.GetComponent<Enemy>().Speed = Random.Range(MinSpeed, MaxSpeed);
+            ShouldIncreaseDifficulty();
         }
 
     }
+
+    private float nextDifficulty;
+    void ShouldIncreaseDifficulty()
+    {
+        if (Player.Score > nextDifficulty)
+        {
+            this.MaxSpeed += DifficultyIncrement;
+            this.MinSpeed += DifficultyIncrement;
+            nextDifficulty += DifficultyEveryScore;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
