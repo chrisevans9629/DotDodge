8c63e19c40e3de561e95e07564f1cf45562fca5b
Chris Evans <chrisevans9629@gmail.com>
2020-03-16 19:20:43 -0500

    added bullets

--- DotDodge/Assets/Bullet.cs
+++ DotDodge/Assets/Bullet.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     public float Speed;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +15,16 @@ public class Enemy : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Bullet"))
+        if (other.CompareTag("Enemy"))
         {
+            Destroy(other.gameObject);
             Destroy(this.gameObject);
         }
     }
 
-
-
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left * Speed * Time.deltaTime;
+        transform.position += Vector3.right * Speed * Time.deltaTime;
     }
 }
--- DotDodge/Assets/Player.cs
+++ DotDodge/Assets/Player.cs
@@ -11,24 +11,38 @@ public class Player : MonoBehaviour
     public float Jump;
     //public float Speed;
     public Rigidbody rb;
+    public GameObject FirePosition;
+    public float FireRateSeconds;
+
+    public GameObject Bullet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Fire());
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (!other.CompareTag("Bullet"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
-
+    IEnumerator Fire()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(FireRateSeconds);
+            Instantiate(Bullet, FirePosition.transform.position, Quaternion.identity);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-         {
-            rb.AddForce(0, Jump * Time.deltaTime,0, ForceMode.Impulse);
+        {
+            rb.AddForce(0, Jump * Time.deltaTime, 0, ForceMode.Impulse);
         }
 
         Score++;
