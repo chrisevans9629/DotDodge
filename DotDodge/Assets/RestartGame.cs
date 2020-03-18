fe7d7809314ca6bb0258df4139db920489ca78b6
Chris Evans <chrisevans9629@gmail.com>
2020-03-17 19:02:57 -0500

    added icons and auto letterbox

--- DotDodge/Assets/RestartGame.cs
+++ DotDodge/Assets/RestartGame.cs
@@ -16,6 +16,11 @@ public class RestartGame : MonoBehaviour
         Time.timeScale = 0;
         this.gameObject.SetActive(true);
     }
+
+    public void GiveUp()
+    {
+        Application.Quit();
+    }
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
