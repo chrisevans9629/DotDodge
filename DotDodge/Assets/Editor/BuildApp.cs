782a7c896a42bfb7998565e705a14752aa075e62
Chris Evans <chrisevans9629@gmail.com>
2020-03-21 13:09:16 -0500

    build app menu now works

--- DotDodge/Assets/Editor/BuildApp.cs
+++ DotDodge/Assets/Editor/BuildApp.cs
@@ -9,9 +9,38 @@ namespace Assets.Editor
         public static void SwitchToPlagueDodge()
         {
             Switch(false,true);
-            
-            
+            var packageName = "com.evanssoftware.plaguedodge";
+            var productName = "Plague Dodge";
+            UpdateBuildSettings(packageName, productName);
         }
+        [MenuItem("BuildApp/Switch To DotDodge")]
+        public static void SwitchToDotDodge()
+        {
+            Switch(true, false);
+            UpdateBuildSettings("com.evanssoftware.dotdodge", "Dot Dodge");
+        }
+
+        [MenuItem("BuildApp/Increment Version")]
+        public static void IncrementVersion()
+        {
+            PlayerSettings.Android.bundleVersionCode++;
+            PlayerSettings.bundleVersion = "0." + PlayerSettings.Android.bundleVersionCode;
+        }
+
+        [MenuItem("BuildApp/Build")]
+        public static void Build()
+        {
+           // BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, )
+        }
+
+        private static void UpdateBuildSettings(string packageName, string productName)
+        {
+            PlayerSettings.productName = productName;
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, packageName);
+            PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Standalone, packageName);
+        }
+
+        
 
         private static void Switch(bool dotDodge, bool plagueDodge)
         {
@@ -30,10 +59,6 @@ namespace Assets.Editor
             EditorBuildSettings.scenes = array;
         }
 
-        [MenuItem("BuildApp/Switch To DotDodge")]
-        public static void SwitchToDotDodge()
-        {
-            Switch(true,false);
-        }
+       
     }
 }
