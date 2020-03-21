b79f7c144ea687cad1a364270aa5d4990b28578b
Chris Evans <chrisevans9629@gmail.com>
2020-03-21 12:26:05 -0500

    added builder settings

---
+++ DotDodge/Assets/Editor/BuildApp.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    public class BuildApp
+    {
+        [MenuItem("BuildApp/Switch To Plague Dodge")]
+        public static void SwitchToPlagueDodge()
+        {
+            Switch(false,true);
+            
+            
+        }
+
+        private static void Switch(bool dotDodge, bool plagueDodge)
+        {
+            var array = EditorBuildSettings.scenes.ToArray();
+            foreach (var editorBuildSettingsScene in array)
+            {
+                if (editorBuildSettingsScene.path.Contains("Scenes/DotDodge"))
+                {
+                    editorBuildSettingsScene.enabled = dotDodge;
+                }
+                else if (editorBuildSettingsScene.path.Contains("Scenes/PlagueDodge"))
+                {
+                    editorBuildSettingsScene.enabled = plagueDodge;
+                }
+            }
+            EditorBuildSettings.scenes = array;
+        }
+
+        [MenuItem("BuildApp/Switch To DotDodge")]
+        public static void SwitchToDotDodge()
+        {
+            Switch(true,false);
+        }
+    }
+}
