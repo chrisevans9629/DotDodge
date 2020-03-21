0662f3da3aa1954b6aa1911fb7af138c5e5187a4
Chris Evans <chrisevans9629@gmail.com>
2020-03-21 16:33:02 -0500

    build now switches icons

--- DotDodge/Assets/Editor/BuildApp.cs
+++ DotDodge/Assets/Editor/BuildApp.cs
@@ -1,23 +1,31 @@
 ﻿using System.Linq;
 using UnityEditor;
-
+using UnityEditor.Android;
+using UnityEngine;
+#if UNITY_EDITOR || UNITY_EDITOR_64 || UNITY_EDITOR_WIN
 namespace Assets.Editor
 {
-    public class BuildApp
+    public class BuildApp : EditorWindow
     {
+        [MenuItem("BuildApp/Open Settings")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow<BuildApp>();
+        }
+
         [MenuItem("BuildApp/Switch To Plague Dodge")]
         public static void SwitchToPlagueDodge()
         {
             Switch(false,true);
             var packageName = "com.evanssoftware.plaguedodge";
             var productName = "Plague Dodge";
-            UpdateBuildSettings(packageName, productName);
+            UpdateBuildSettings(packageName, productName,"PlagueDodge");
         }
         [MenuItem("BuildApp/Switch To DotDodge")]
         public static void SwitchToDotDodge()
         {
             Switch(true, false);
-            UpdateBuildSettings("com.evanssoftware.dotdodge", "Dot Dodge");
+            UpdateBuildSettings("com.evanssoftware.dotdodge", "Dot Dodge", "DotDodge");
         }
 
         [MenuItem("BuildApp/Increment Version")]
@@ -30,14 +38,41 @@ namespace Assets.Editor
         [MenuItem("BuildApp/Build")]
         public static void Build()
         {
-           // BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, )
+            //BuildPipeline.BuildPlayer(new BuildPlayerOptions(){});
         }
 
-        private static void UpdateBuildSettings(string packageName, string productName)
+        //public Texture2D Icon;
+        //public Texture2D DotIcons;
+        //void OnGUI()
+        //{
+        //    Icon = (Texture2D) EditorGUILayout.ObjectField(Icon, typeof(Texture2D), false);
+        //    DotIcons = (Texture2D) EditorGUILayout.ObjectField(DotIcons, typeof(Texture2D), false);
+        //}
+        private static void UpdateBuildSettings(string packageName, string productName, string iconFolder)
         {
             PlayerSettings.productName = productName;
             PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, packageName);
             PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Standalone, packageName);
+
+            var result = PlayerSettings.GetPlatformIcons(BuildTargetGroup.Android, AndroidPlatformIconKind.Adaptive);
+
+            var texture = AssetDatabase.LoadAssetAtPath<Texture2D>($"Assets/Screenshots/{iconFolder}/icon.png");
+
+            result.First().SetTextures(new []{texture,texture});
+            //result.SetTexture();
+
+            var result2 = PlayerSettings.GetPlatformIcons(BuildTargetGroup.Android, AndroidPlatformIconKind.Legacy);
+
+            result2.First().SetTexture(texture);
+
+            var result3 = PlayerSettings.GetPlatformIcons(BuildTargetGroup.Android, AndroidPlatformIconKind.Round);
+            result3.First().SetTexture(texture);
+
+
+            PlayerSettings.SetPlatformIcons(BuildTargetGroup.Android, AndroidPlatformIconKind.Adaptive, result);
+            PlayerSettings.SetPlatformIcons(BuildTargetGroup.Android, AndroidPlatformIconKind.Legacy, result2);
+            PlayerSettings.SetPlatformIcons(BuildTargetGroup.Android, AndroidPlatformIconKind.Round, result3);
+            //PlayerSettings.SetPlatformIcons(BuildTargetGroup.Android, null, new []{new PlatformIcon(), });
         }
 
         
@@ -62,3 +97,4 @@ namespace Assets.Editor
        
     }
 }
+#endif
