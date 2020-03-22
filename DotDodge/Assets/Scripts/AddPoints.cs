bb9e71a0f78e5c0b8077da06d790cad2ebea4981
Chris Evans <chrisevans9629@gmail.com>
2020-03-22 08:47:19 -0500

    add points when enemies are killed

---
+++ DotDodge/Assets/Scripts/AddPoints.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class AddPoints : MonoBehaviour
+    {
+        public float PointValue;
+        public PlayerBase Player;
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Enemy"))
+            {
+                Player.Score += PointValue;
+            }
+        }
+    }
+}
