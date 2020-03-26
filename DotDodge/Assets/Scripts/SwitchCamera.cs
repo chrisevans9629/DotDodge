481ab829f579d28553887aa678a5893c90aebd50
Chris Evans <chrisevans9629@gmail.com>
2020-03-25 20:43:01 -0500

    change to 3d is now animated

--- DotDodge/Assets/Scripts/SwitchCamera.cs
+++ DotDodge/Assets/Scripts/SwitchCamera.cs
@@ -11,19 +11,34 @@ namespace Assets.Scripts
         public bool Is3D;
         public void SwitchTo3D()
         {
-            Follow.IsFollowingActive = true;
-            Camera.transform.position = Position3D;
+            Camera.fieldOfView = 80;
             Camera.orthographic = false;
-            Camera.transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
-            Follow.SetupOffset();
+
+           // Camera.transform.position = Position3D;
+           // Camera.transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
+
+            LeanTween.value(gameObject, vector3 => Camera.transform.position = vector3, Camera.transform.position,
+                Position3D, 1);
+            LeanTween.value(gameObject, f => Camera.transform.rotation = Quaternion.AngleAxis(f, Vector3.up),
+                Camera.transform.rotation.eulerAngles.y, 90, 1).setOnComplete(() =>
+            {
+                Follow.IsFollowingActive = true;
+                Follow.SetupOffset();
+            });
+
+            
         }
 
         public void SwitchTo2D()
         {
             Follow.IsFollowingActive = false;
-            Camera.transform.position = Position2D;
-            Camera.transform.rotation = Quaternion.identity;
-            Camera.orthographic = true;
+            //Camera.transform.position = Position2D;
+            //Camera.transform.rotation = Quaternion.identity;
+            //Camera.orthographic = true;
+            LeanTween.value(gameObject, vector3 => Camera.transform.position = vector3, Camera.transform.position,
+                Position2D, 1);
+            LeanTween.value(gameObject, f => Camera.transform.rotation = Quaternion.AngleAxis(f, Vector3.up),
+                Camera.transform.rotation.eulerAngles.y, 0, 1).setOnComplete(() => { Camera.orthographic = true; });
         }
 
         public void Toggle()
