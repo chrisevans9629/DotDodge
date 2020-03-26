d639843bd6a2dc19031ba50509e7f80df1c8da5d
Chris Evans <chrisevans9629@gmail.com>
2020-03-25 20:34:32 -0500

    added ability to switch camera

---
+++ DotDodge/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera Camera;
+    public float Speed;
+    public Transform Follow;
+    public bool IsFollowingActive = true;
+    public Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupOffset();
+    }
+
+    public void SetupOffset()
+    {
+        if (Follow != null)
+        {
+            Offset = Camera.transform.position - Follow.position;
+        }
+    }
+    private Vector3 velocity;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Follow != null && IsFollowingActive)
+            Camera.transform.position = Vector3.SmoothDamp(Camera.transform.position, Follow.position + Offset, ref velocity, Speed);
+    }
+}
---
+++ DotDodge/Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SwitchCamera : MonoBehaviour
+    {
+        public CameraFollow Follow;
+        public Camera Camera;
+        public Vector3 Position2D;
+        public Vector3 Position3D;
+        public bool Is3D;
+        public void SwitchTo3D()
+        {
+            Follow.IsFollowingActive = true;
+            Camera.transform.position = Position3D;
+            Camera.orthographic = false;
+            Camera.transform.rotation = Quaternion.AngleAxis(90, Vector3.up);
+            Follow.SetupOffset();
+        }
+
+        public void SwitchTo2D()
+        {
+            Follow.IsFollowingActive = false;
+            Camera.transform.position = Position2D;
+            Camera.transform.rotation = Quaternion.identity;
+            Camera.orthographic = true;
+        }
+
+        public void Toggle()
+        {
+            if (Is3D)
+            {
+                SwitchTo2D();
+            }
+            else
+            {
+                SwitchTo3D();
+            }
+
+            Is3D = !Is3D;
+        }
+    }
+}
