c0667e48d8b119e4785d9deed30013a2304ece44
Chris Evans <chrisevans9629@gmail.com>
2020-03-28 18:55:09 -0500

    fixed sonar going off screen forever.  added a logo

---
+++ DotDodge/Assets/Scripts/DestroyDelay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class DestroyDelay : MonoBehaviour
+    {
+        public float Delay;
+
+        void Start()
+        {
+            Destroy(gameObject, Delay);
+        }
+    }
+}
